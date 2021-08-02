f8e9696b619cb75f35cac352602bfbb5c21a3ba1
Mbusiseni Mbhele <mbusiseni.mbhele@gmail.com>
2021-08-02 20:33:21 +0200

    Initial commit was done

---
+++ app.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector('button');
+ const input = document.querySelector('#binaryNumber').value;
+
+btn.addEventListener('click', binaryAgent )
+
+
+function binaryAgent() {
+    const input = document.querySelector('#binaryNumber').value;
+    var biString = input.split(" ");
+    var uniString = [];
+  
+    /*using the radix (or base) parameter in parseInt, we can convert the binary
+        number to a decimal number while simultaneously converting to a char*/
+  
+    for (var i = 0; i < biString.length; i++) {
+      uniString.push(String.fromCharCode(parseInt(biString[i], 2)));
+    }
+    // console.log(decodeURIComponent(JSON.parse('"' + uniString.replace(/\"/g, '\\"') + '"')))
+
+   console.log(uniString.join(""))
+    // we then simply join the string
+    // console.log(decodeURIComponent(JSON.parse('"' + uniString.replace(/\"/g, '\\"') + '"')))
+    document.getElementById('output').innerHTML=`<br><p>Result :${uniString.join("")}</p>`;
+    return uniString.join("");
+    
+  }
+  
