354d9151234ab7ac33b4d12b872d9b34d223e918
mbusiseni mbhele <mbusiseni.mbhele@gmail.com>
2022-03-28 12:41:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-const btn = document.querySelector('button');
+ //GLOBAL VARIABLES 
+ const btn = document.querySelector('button');
  const input = document.querySelector('#binaryNumber').value;
 
-btn.addEventListener('click', binaryAgent )
+ //ADD EVENT LISTEN TO THE BUTTON
+ btn.addEventListener('click', binaryAgent)
 
+ //MAIN FUNCTION OF THE APPLICATION
+ function binaryAgent() {
 
-function binaryAgent() {
-    const input = document.querySelector('#binaryNumber').value;
-    var biString = input.split(" ");
-    var uniString = [];
-  
-    /*using the radix (or base) parameter in parseInt, we can convert the binary
-        number to a decimal number while simultaneously converting to a char*/
-  
-    for (var i = 0; i < biString.length; i++) {
-      uniString.push(String.fromCharCode(parseInt(biString[i], 2)));
-    }
-    // console.log(decodeURIComponent(JSON.parse('"' + uniString.replace(/\"/g, '\\"') + '"')))
+     //       TAKE THE CLIENT INPUT 
+     const input = document.querySelector('#binaryNumber').value;
+     var biString = input.split(" ");
+     var uniString = [];
 
-   console.log(uniString.join(""))
-    // we then simply join the string
-    // console.log(decodeURIComponent(JSON.parse('"' + uniString.replace(/\"/g, '\\"') + '"')))
-    document.getElementById('output').innerHTML=`<br><p>Result :${uniString.join("")}</p>`;
-    return uniString.join("");
-    
-  }
-  
+     /*using the radix (or base) parameter in parseInt, we can convert the binary
+         number to a decimal number while simultaneously converting to a char*/
+
+     for (var i = 0; i < biString.length; i++) {
+         uniString.push(String.fromCharCode(parseInt(biString[i], 2)));
+     }
+     // console.log(decodeURIComponent(JSON.parse('"' + uniString.replace(/\"/g, '\\"') + '"')))
+
+     console.log(uniString.join(""))
+     // we then simply join the string
+     // console.log(decodeURIComponent(JSON.parse('"' + uniString.replace(/\"/g, '\\"') + '"')))
+     document.getElementById('output').innerHTML = `<br><p>Result :${uniString.join("")}</p>`;
+     return uniString.join("");
+
+ }
